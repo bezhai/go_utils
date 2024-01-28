ab60f4b049c4bc8df3c6bd700ac02ef665a42664
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-01-28 18:56:36 +0800

    fix error

--- httpx/auth.go
+++ httpx/auth.go
@@ -9,35 +9,33 @@ import (
 	"github.com/bezhai/go_utils/authx"
 )
 
-func ServerAuthMiddleware(secret string) func(next app.HandlerFunc) app.HandlerFunc {
-	return func(next app.HandlerFunc) app.HandlerFunc {
-		return func(c context.Context, ctx *app.RequestContext) {
-			salt := ctx.Request.Header.Get("X-Salt")
-			if salt == "" {
-				ctx.String(http.StatusForbidden, "missing salt")
-				return
-			}
-
-			clientToken := ctx.Request.Header.Get("X-Token")
-			if clientToken == "" {
-				ctx.String(http.StatusForbidden, "missing token")
-				return
-			}
-
-			bodyBytes, err := ctx.Body()
-			if err != nil {
-				ctx.String(http.StatusForbidden, "cannot read body")
-				return
-			}
-
-			serverToken := authx.GenerateToken(salt, string(bodyBytes), secret)
-
-			if serverToken != clientToken {
-				ctx.String(http.StatusForbidden, "invalid token")
-				return
-			}
-
-			next(c, ctx)
+func ServerAuthMiddleware(secret string) app.HandlerFunc {
+	return func(c context.Context, ctx *app.RequestContext) {
+		salt := ctx.Request.Header.Get("X-Salt")
+		if salt == "" {
+			ctx.String(http.StatusForbidden, "missing salt")
+			return
 		}
+
+		clientToken := ctx.Request.Header.Get("X-Token")
+		if clientToken == "" {
+			ctx.String(http.StatusForbidden, "missing token")
+			return
+		}
+
+		bodyBytes, err := ctx.Body()
+		if err != nil {
+			ctx.String(http.StatusForbidden, "cannot read body")
+			return
+		}
+
+		serverToken := authx.GenerateToken(salt, string(bodyBytes), secret)
+
+		if serverToken != clientToken {
+			ctx.String(http.StatusForbidden, "invalid token")
+			return
+		}
+
+		ctx.Next(c)
 	}
 }
